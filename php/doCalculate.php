800f0153500bef73b643d77f2539dc2770ca8dbb
mhy1264 <s710516@student2.ylsh.chc.edu.tw>
2022-01-17 23:26:59 +0800

    finish doCal()

---
+++ php/doCalculate.php
@@ -0,0 +1,32 @@
+<?php
+
+    $buyPrice=$_POST['buyPrice'];
+    $sellPrince=$_POST['sellPrice'];
+    $unit=$_POST['unit'];
+    $num=$_POST['num'];
+    $diff=($sellPrince-$buyPrice)*1000*num;
+    if($diff>=0)
+    {
+        print("<p>你總共賺的錢是".$diff."元</p>");
+        print("<p>總共可以換".$diff/$unit);
+        if($unit==10)
+            print("瓶麥香");
+        else if ($unit==160)
+            print("杯星巴克冷翠咖啡");
+        else if ($unit==75)
+            print("個麥當勞大麥克");
+        else if ($unit==25250)
+            print("個月的年終(依照基本時薪計算");
+        else if($unit==168)
+            print("個小時的薪水");
+        print("</p>");
+
+    }
+    else 
+    {
+        print("<p>可悲阿 被收割ㄌ顆顆</p>");
+    }
+
+
+
+?>
--- php/showCalculate.php
+++ php/showCalculate.php
@@ -2,15 +2,17 @@
 
 print("<form>");
 print('<p>輸入購買的金額</p>');
-print('<label><input type="text">購買金額');
-print('<label><input type="text">賣出金額');
+print('<p><label>購買金額<input type="text" id="inPrice">');
+print('<label>賣出金額<input type="text" id="outPrice"></p>');
+print('<label>持有張數<input type="text" id="num"></p>');
+print('<p><label>想要換算的東西<select id="thing">');
+print('<option value="10">麥香10元系列</option>');
+print('<option value="160">星巴克冷翠咖啡</option>');
+print('<option value="75">麥當勞大麥克</option>');
+// print('<option value="10">麥香10元系列</option>');
+print('<option value="25250">基本月薪</option>');
+print('<option value="168">基本時薪</option></p>');
 print('<label><input type="button" onclick="doCal()" value="Submit"></label>');
 print('</form>');
 print('<div id="showCalResult"></div>');
-
-
-
-
-
-
 ?>
