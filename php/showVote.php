97aedc7c44ab012dd49e17b9d5148f167174f439
yixiang0706 <mark104157@gmail.com>
2022-01-18 20:50:51 +0800

    modify button

--- php/showVote.php
+++ php/showVote.php
@@ -1,11 +1,9 @@
 <?php
 print("<form>");
-print('<p>預測下一個交易日會漲還是跌</p>');
+print('<p><h2>預測下一個交易日會漲還是跌</h2></p>');
 print('<label><input type="radio" name="V" value="U">會漲');
 print('<input type="radio" name="V" value="D">會跌</label>');
-print('<label><input type="button" onclick="doVote()" value="Submit"></label>');
+print('<br><label><input type="button" onclick="doVote()" value="Submit" class="button"></label>');
 print('</form>');
 print('<div id="showVoteResult"></div>');
-
-
 ?>
